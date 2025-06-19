9676e8fb08d6137b2b06e4269e9789503a53a56f
Your Name <your_email@example.com>
2025-06-19 23:18:17 +0530

    Completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  server: {
+    port: 5000,         // change this to your desired port
+    strictPort: true    // (optional) fail if 5000 is taken, don't auto-increment
+  },
   plugins: [react()],
   resolve: {
     alias: {

eb4cdaf1b42b90f8cae8e863143025c24156df97
Gaytri Tripathi <beccaa461@gmail.com>
2025-06-26 00:48:33 +0530

    Update constants.js

--- frontend/src/shared/constants.js
+++ frontend/src/shared/constants.js
@@ -1,5 +1,4 @@
 export const API_BASE_URL = import.meta.env.MODE === "development" ? 'http://localhost:5000/api/' : "/api"
-// src/modules/community/constants.js
 export const COMMUNITY_TYPES = [
   'Education',
   'Environment',
